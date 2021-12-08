b3d92dd625302fdc8eee163c523514b34d7fce26
Lucas Maieski <lucmaieski@gmail.com>
2021-12-08 18:06:08 -0300

    Feito pagina de login

---
+++ src/pages/Foods.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Foods() {
+  return (
+    <div>
+      Food Page
+    </div>
+  );
+}
+
+export default Foods;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from '../pages/Foods';
 import Login from '../pages/Login';
 
 function Routes() {
   return (
     <Switch>
+      <Route path="/comidas" component={ Header } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
