5758c784c2dda18a3cd5aa0e9248aa78bd3c0d6b
lukzgs <lucgs88@gmail.com>
2021-12-11 18:09:53 -0300

    delete this commit

--- src/buttons/button.jsx
+++ src/buttons/button.jsx
@@ -1,34 +1,58 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import { BUTTON_CAT } from '../global/constants';
 import foodApiToSelect from '../services/searchMeals';
 import drinkApiToSelect from '../services/searchDrinks';
+import MyContext from '../context/MyContext';
 
-function Button({ categories }) {
+export default function Button({ categories }) {
+  const history = useHistory();
+  const { setMeals, setDrinks } = useContext(MyContext);
 
-  function clickHandler() {
-    const fetchApi = async () => {
-      if (history.pathname === '/comidas') {
-        const response = await foodApiToSelect('ALL');
-        setMeals(response.meals);
-      }
-
-      const response = await foodApiToSelect('ALL');
-      setMeals(response.meals);
-    };
+  function clickHandler(categorie) {
+    // let api = [];
+    // if (history.location.pathname === '/comidas') {
+    //   const fetchApi = async () => {
+    //     const response = await foodApiToSelect('CATEGORIES', categorie);
+    //     console.log(response);
+    //     setMeals(response.meals);
+    //     api = response;
+    //     return fetchApi;
+    //   };
+    // }
+    // if (history.location.pathname === '/bebidas') {
+    //   const fetchApi = async () => {
+    //     const response = await drinkApiToSelect('CATEGORIES', categorie);
+    //     setDrinks(response.drinks);
+    //     api = response;
+    //     return fetchApi;
+    //   };
+    // }
+    return console.log(categorie);
   }
 
   const buttons = categories.filter((card, index) => index <= BUTTON_CAT);
+
   return (
-    buttons.map(({ strCategory }, index) => (
-      <input
-        key={ index }
-        data-testid={ `${strCategory}-category-filter` }
-        type="button"
-        alt={ strCategory }
-        value={ strCategory }
-      />
-    ))
+    <div>
+      { buttons.map(({ strCategory }, index) => (
+        <input
+          key={ index }
+          data-testid={ `${strCategory.split(' ').splice('/')}-category-filter` }
+          type="button"
+          alt={ strCategory }
+          value={ strCategory }
+          onClick={ clickHandler(strCategory) }
+        />
+      ))}
+    </div>
+
   );
 }
 
-export default Button;
+Button.propTypes = {
+  categories: PropTypes.oneOfType(
+    (PropTypes.array, PropTypes.object, PropTypes.func),
+  ).isRequired,
+};
