6e2d1be58815c666f7d7bae1349040de6414bff0
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-01-04 16:03:08 -0300

    Implementa funcionalidades da tela de explorar bebidas ou comidas

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import drinkApiToSelect from '../services/searchDrinks';
 
 function ExploreDrinks({ history }) {
-  function redirectToExplore(type) {
-    history.push(`/explorar/bebidas/${type}`);
+  const [random, setRandom] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { drinks } = await drinkApiToSelect('RANDOM');
+      setRandom(drinks[0].idDrink);
+    })();
+  }, []);
+
+  console.log(random);
+
+  function redirectToExplore(type, explore) {
+    if (explore) {
+      history.push(`/explorar/bebidas/${type}`);
+    } else {
+      history.push(`/bebidas/${type}`);
+    }
   }
   return (
     <div>
@@ -13,14 +29,14 @@ function ExploreDrinks({ history }) {
       <button
         type="button"
         data-testid="explore-by-ingredient"
-        onClick={ () => redirectToExplore('ingredientes') }
+        onClick={ () => redirectToExplore('ingredientes', true) }
       >
         Por Ingredientes
       </button>
       <button
         type="button"
         data-testid="explore-surprise"
-        onClick={ () => redirectToExplore('random') }
+        onClick={ () => redirectToExplore(random, false) }
       >
         Me Surpreenda!
       </button>
--- src/pages/ExploreMeals.jsx
+++ src/pages/ExploreMeals.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import foodApiToSelect from '../services/searchMeals';
 
 function ExploreMeals({ history }) {
-  function redirectToExplore(type) {
-    history.push(`/explorar/comidas/${type}`);
+  const [random, setRandom] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { meals } = await foodApiToSelect('random');
+      setRandom(meals[0].idMeal);
+    })();
+  }, []);
+
+  console.log(random);
+
+  function redirectToExplore(type, explore) {
+    if (explore) {
+      history.push(`/explorar/comidas/${type}`);
+    } else {
+      history.push(`/comidas/${type}`);
+    }
   }
 
   return (
@@ -14,21 +30,21 @@ function ExploreMeals({ history }) {
       <button
         type="button"
         data-testid="explore-by-ingredient"
-        onClick={ () => redirectToExplore('ingredientes') }
+        onClick={ () => redirectToExplore('ingredientes', true) }
       >
         Por Ingredientes
       </button>
       <button
         type="button"
         data-testid="explore-by-area"
-        onClick={ () => redirectToExplore('area') }
+        onClick={ () => redirectToExplore('area', true) }
       >
         Por Local de Origem
       </button>
       <button
         type="button"
         data-testid="explore-surprise"
-        onClick={ () => redirectToExplore('random') }
+        onClick={ () => redirectToExplore(random, false) }
       >
         Me Surpreenda!
       </button>
