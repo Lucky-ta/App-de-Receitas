766a151f04214485ab26d7b2ea6bfe3002bdf753
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-01-05 15:20:19 -0300

    implementa a funcionalidade de filtar por ingredientes

--- src/components/IngredientsDrinksCards.jsx
+++ src/components/IngredientsDrinksCards.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import MyContext from '../context/MyContext';
+import drinkApiToSelect from '../services/searchDrinks';
 
 function IngredientsDrinksCards({ value }) {
+  const history = useHistory();
+  const { setDrinks } = useContext(MyContext);
+  function handleClick(ingredient) {
+    const fetchApi = async () => {
+      const { drinks } = await drinkApiToSelect('INGREDIENT', ingredient);
+      setDrinks(drinks);
+    };
+    fetchApi();
+    history.push('/bebidas');
+  }
   console.log(value);
   return (
     value.map((ingredient, index) => (
       <div
+        role="button"
+        tabIndex={ index }
+        onClick={ () => handleClick(ingredient.strIngredient1) }
+        onKeyPress={ () => handleClick(ingredient.strIngredient1) }
         data-testid={ `${index}-ingredient-card` }
         key={ `${index}${ingredient.strIngredient1}` }
       >
