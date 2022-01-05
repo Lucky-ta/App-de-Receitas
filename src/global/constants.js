b564c8b04b9216c21c4d4fcd0c7108bbc6b4cdb4
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-01-05 17:10:25 -0300

    Implementa funcionalidades de pesquisar por area

--- src/global/constants.js
+++ src/global/constants.js
@@ -1,3 +1,4 @@
 export const MAX_CARDS = 11;
 export const TOTAL_CARDS = 6;
 export const BUTTON_CAT = 4;
+export const TWELVE = 12;
--- src/pages/ExploreMealsByArea.jsx
+++ src/pages/ExploreMealsByArea.jsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import foodApiToSelect from '../services/searchMeals';
+import { TWELVE } from '../global/constants';
 
 function ExploreMealsByArea() {
+  const [areas, setAreas] = useState([]);
+  const [mealsByArea, setMealsByArea] = useState([]);
+  const [optionArea, setOptionArea] = useState();
+  const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      const { meals } = await foodApiToSelect('BYAREA');
+      // const firstMeals = meals.slice(0, 13);
+      setAreas(meals);
+      setOptionArea(meals[0].strArea);
+    })();
+  }, []);
+
+  // console.log(areas);
+  // console.log(optionArea);
+
+  useEffect(() => {
+    (async () => {
+      const { meals } = await foodApiToSelect('area', optionArea);
+      if (meals) {
+        const firstMealsByArea = meals.slice(0, TWELVE);
+        setMealsByArea(firstMealsByArea);
+      }
+    })();
+  }, [optionArea]);
+
+  // console.log(mealsByArea);
+
+  function renderSelect() {
+    return (
+      <select
+        data-testid="explore-by-area-dropdown"
+        value={ optionArea }
+        onChange={ ({ target }) => setOptionArea(target.value) }
+      >
+        {areas.map(({ strArea }) => (
+          <option
+            key={ `${strArea}-key` }
+            data-testid={ `${strArea}-option` }
+            value={ strArea }
+          >
+            {strArea}
+          </option>
+        ))}
+      </select>
+    );
+  }
+
+  function renderCards() {
+    return (
+      <div>
+        {mealsByArea.map(({ strMeal, strMealThumb, idMeal }, index) => (
+          <div
+            role="button"
+            tabIndex={ index }
+            onClick={ () => history.push(`/comidas/${idMeal}`) }
+            onKeyPress={ () => history.push(`/comidas/${idMeal}`) }
+            key={ `${strMeal}-${index}` }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <img
+              width="150px"
+              data-testid={ `${index}-card-img` }
+              src={ strMealThumb }
+              alt={ strMeal }
+            />
+            <h3 data-testid={ `${index}-card-name` }>{strMeal}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header title="Explorar Origem" isRender />
+      {renderSelect()}
+      {renderCards()}
       <Footer />
     </div>
   );
